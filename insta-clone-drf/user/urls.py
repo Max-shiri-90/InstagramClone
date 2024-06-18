662d4d127e33533cdebba54d98ce92496be8999c
max <max@localhost.localdomain>
2024-06-18 19:35:36 +0330

    uncoment

--- insta-clone-drf/user/urls.py
+++ insta-clone-drf/user/urls.py
@@ -1,10 +1,10 @@
-# from django.urls import path
+from django.urls import path
 
-# from .views import RegisterViewAPI, LoginViewAPI
+from .views import RegisterViewAPI, LoginViewAPI
 
 
-# urlpatterns = [
-#     path('register/<int:pk>', RegisterViewAPI.as_view(), name='register'),
-#     path('login/<int:pk>', LoginAPI.as_view(), name='login'),
-#     # path('logout/', knox_views.LogoutView.as_view(), name='logout'),
-# ]
+urlpatterns = [
+    path('register/<int:pk>', RegisterViewAPI.as_view(), name='register'),
+    path('login/<int:pk>', LoginAPI.as_view(), name='login'),
+    # path('logout/', knox_views.LogoutView.as_view(), name='logout'),
+]
